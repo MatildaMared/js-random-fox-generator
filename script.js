e732499e6e3cfc54b19a22b85f796a0509937ae5
MatildaMared <matildamared@live.se>
2021-03-17 15:57:00 +0100

    fix functionality

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const imageContainer = document.querySelector(".fox-image");
+const btn = document.querySelector(".btn");
 
 const renderImage = function (url) {
+    imageContainer.innerHTML = "";
     const imgElement = document.createElement("img");
     imgElement.alt = "A cute picture of a fox";
     imgElement.src = url;
     imageContainer.insertAdjacentElement("beforeend", imgElement);
 }
 
-fetch("https://randomfox.ca/floof/")
-    .then(response => response.json())
-    .then(data => {
-        renderImage(data.image);
-    });
+const generateRandomFox = function () {
+    fetch("https://randomfox.ca/floof/")
+        .then((response) => response.json())
+        .then((data) => {
+            renderImage(data.image);
+        });
+}
+
+btn.addEventListener("click", generateRandomFox);
+
+generateRandomFox();
